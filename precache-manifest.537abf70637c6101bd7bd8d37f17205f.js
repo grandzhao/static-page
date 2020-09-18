9b5c3dab9fb7432dd18d0d394d9aa2ea521d8a3d
Grand Zhao <grand.zhao@eventbank.com>
2020-09-18 17:35:33 +0800

    Updates

---
+++ precache-manifest.537abf70637c6101bd7bd8d37f17205f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3903846bddbce1966122f3e80262c6a5",
+    "url": "/static-page/index.html"
+  },
+  {
+    "revision": "2bb0e7514be5cf6657ae",
+    "url": "/static-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01e3abb05bcf0a034540",
+    "url": "/static-page/static/js/2.80244b21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-page/static/js/2.80244b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb0e7514be5cf6657ae",
+    "url": "/static-page/static/js/main.5a1fe922.chunk.js"
+  },
+  {
+    "revision": "13614d96aaa88b40466f",
+    "url": "/static-page/static/js/runtime-main.d888a8ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
